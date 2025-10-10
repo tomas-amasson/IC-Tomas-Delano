77d25c83c1c6fe9abf39ef4ca1c14b6f618b8dc8
tomas-amasson <massontomas18@gmail.com>
2025-10-09 22:01:22 -0300

    funções acessorias implementadas

--- testes/class_selector.py
+++ testes/class_selector.py
@@ -39,7 +39,7 @@ class Message(db.Model):
     resourceID = db.Column(db.Integer, nullable= False)
 
     __mapper_args__ = {
-        'polymorphic_identity': "message",
+        'polymorphic_identity': "Message",
         'polymorphic_on': message_type
     }
 
@@ -293,12 +293,11 @@ def select(data):
     #informação de entrada
 
     #criação da classe selecionada
-    messages = {"Message": lambda: (Message.extract_message(data), Message_Schema), "PlayGameMessage": lambda: (PlayGameMessage.extract(data), PlayGameMessage_Schema), "GameModeMessage": lambda: (GameModeMessage.extract(data), GameModeMessage_Schema), "CaseSelectedMessage": lambda: (CaseSelectedMessage.extract(data), CaseSelectedMessage_Schema), "PowerUpMessage": lambda: (PowerUpMessage.extract(data), PowerUpMessage_Schema), "CaseDetailsMessage": lambda: (CaseDetailsMessage.extract(data), CaseDetailsMessage_Schema), "WordSendMessage": lambda: (WordSendMessage.extract(data), WordSendMessage_Schema), "WordValidationMessage": lambda: (WordValidationMessage.extract(data), WordValidationMessage_Schema)}
+    messages = {"Message": lambda: (Message, Message_Schema), "PlayGameMessage": lambda: (PlayGameMessage, PlayGameMessage_Schema), "GameModeMessage": lambda: (GameModeMessage, GameModeMessage_Schema), "CaseSelectedMessage": lambda: (CaseSelectedMessage, CaseSelectedMessage_Schema), "PowerUpMessage": lambda: (PowerUpMessage, PowerUpMessage_Schema), "CaseDetailsMessage": lambda: (CaseDetailsMessage, CaseDetailsMessage_Schema), "WordSendMessage": lambda: (WordSendMessage, WordSendMessage_Schema), "WordValidationMessage": lambda: (WordValidationMessage, WordValidationMessage_Schema)}
     
     
     cms, schm = messages[data['message_type']]()
     return cms, schm
-    
 
 
 @app.route('/api', methods= ['GET'])
@@ -307,7 +306,50 @@ def get():
     message_schema = Message_Schema(many= True)
 
     messages = message_schema.dump(all_messages)
+
+    idx = 0
+    for e in messages:
+        cms, schm = select(e)
+        specific = cms.query.filter(cms.id == e['id']).all()
+        specific = schm(many= True).dump(specific)
+        print(specific, schm)
+        
+        messages[idx] = specific[0]
+        idx += 1 
+
     return make_response(jsonify({"messages": messages}))
+
+
+@app.route('/api/<int:id>', methods= ['GET'])
+def get_id(id: int):
+    message_id = Message.query.filter(Message.id == id).first()
+    message_schema = Message_Schema()
+
+    father_message = message_schema.dump(message_id)
+
+    if (message_id == None):
+        return make_response(f"Message not found.\nid = {id}")
+
+    cms, schm = select(father_message)
+    specific_messages = cms.query.filter(cms.id == id).first()
+    children_message = schm().dump(specific_messages)
+
+    return make_response(jsonify({"message": children_message | father_message}))
+
+@app.route('/api/<int:id>', methods= ['DELETE'])
+def delete_id(id: int):
+    general = Message.query.filter(Message.id == id).first()
+    dumped = Message_Schema().dump(general)
+
+    cms, schm = select(dumped)
+    specific = cms.query.filter(cms.id == id).first()
+    sdumped = schm().dump(specific)
+    
+    db.session.delete(general)
+    db.session.commit()
+
+    return make_response(jsonify([dumped, sdumped]))
+
 
 @app.route('/api', methods= ['POST'])
 def post():
@@ -322,6 +364,30 @@ def post():
 
     result = message_schema.dump(message)
     return make_response(jsonify({"message": result}), 201)
+
+@app.route('/api/<int:id>', methods= ['PUT'])
+def put(id: int):
+    update = request.get_json()
+
+    general = Message.query.filter(Message.id == id).first()
+    dumped = Message_Schema().dump(general)
+
+    cms, schm = select(dumped)
+    specific = cms.query.filter(cms.id == id).first()
+    sdumped = schm().dump(specific)
+
+    for att in cms.__dict__.keys():
+        if (update.get(att)):
+            sdumped[att] = update.get(att)
+
+    # remove id -> não controlado pelo usuario
+    del sdumped['id']
+    
+    schm().load(sdumped, instance= specific, session= db.session)
+    db.session.commit()
+
+    return make_response(jsonify([sdumped]))
+
 
 
 if __name__ == "__main__":
