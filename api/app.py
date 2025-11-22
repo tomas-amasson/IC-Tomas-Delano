cebc200730e09a5e9e42d26ba1cfdc6724902b60
Delano Medeiros Beder <delanobeder@yahoo.com.br>
2025-11-22 10:37:10 -0300

    Atualização

--- api/app.py
+++ api/app.py
@@ -415,8 +415,5 @@ def put(id: int):
     return make_response(jsonify([sdumped]))
 
 
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug= True)
+with app.app_context():
+    db.create_all()
